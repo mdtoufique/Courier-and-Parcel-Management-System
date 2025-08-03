f55c3e46242cb7a3529c464a51de596425dce55b
toufique <mdrehmant@gmail.com>
2025-08-03 18:18:32 +0600

    cors update

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5174',
     'http://192.168.0.106:5173',
+    'https://courier-and-parcel-management-system-1ojffshrb.vercel.app',
   ],
  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   credentials: true, 
