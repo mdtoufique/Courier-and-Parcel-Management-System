c43c8680ad912db7899df8a1318660b645fb2b43
toufique <mdrehmant@gmail.com>
2025-08-03 19:59:48 +0600

    bug fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,8 +8,9 @@ const JWT_EXPIRES_IN = "30d"
 
 export const register = async (req, res) => {
   try {
+   
     const { name, email, password,role, phone } = req.body;
-
+ console.log(role);
     const existing = await User.findOne({ email });
     if (existing) return res.status(400).json({ message: "Email already registered" });
 
